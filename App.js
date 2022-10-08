3af606572e56514a650c76412d29f5639431d579
ayushmanbajpayee <87260134+ayushmanbajpayee@users.noreply.github.com>
2022-10-08 09:36:52 +0530

    Update App.js

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 const App = () => {
   return (
-    <Box width="500px" sx={{ width: { xl: '1500px' } }} m="manual">
+    <Box width="489px" sx={{ width: { xl: '1479px' } }} m="auto">
     <Navbar />
     <Routes>
       <Route path="/" element={<Home />} />
